ea109e047b6346c1b437f22ed834e3ec9cb74e3b
Your Name <you@example.com>
2024-01-29 15:32:14 +0100

    minor update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Amplify } from "aws-amplify";
-/* import awsConfig from "./aws-exports"; */
+import awsConfig from "./aws-exports";
 import { Authenticator } from "@aws-amplify/ui-react";
 console.log("hej", process.env.REACT_APP_USER_POOL_ID);
 
-const awsConfig = {
+/* const awsConfig = {
   Auth: {
     region: process.env.REACT_APP_USER_POOL_REGION,
     userPoolId: process.env.REACT_APP_USER_POOL_ID,
@@ -16,7 +16,7 @@ const awsConfig = {
   Analytics: {
     disabled: true,
   },
-};
+}; */
 console.log("awsConfig", awsConfig);
 Amplify.configure(awsConfig);
 
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,7 +2,7 @@ const awsExports = {
   Auth: {
     region: process.env.REACT_APP_USER_POOL_REGION,
     userPoolId: process.env.REACT_APP_USER_POOL_ID,
-    userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+    userPoolWebClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID,
     authenticationFlowType: "USER_SRP_AUTH",
   },
   Analytics: {
