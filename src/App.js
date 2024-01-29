491e1171b10e3a584d4668cafd18055a8373865e
Your Name <you@example.com>
2024-01-29 15:28:49 +0100

    minor update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const awsConfig = {
   Auth: {
     region: process.env.REACT_APP_USER_POOL_REGION,
     userPoolId: process.env.REACT_APP_USER_POOL_ID,
-    userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+    userPoolWebClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID,
     authenticationFlowType: "USER_SRP_AUTH",
   },
   Analytics: {
